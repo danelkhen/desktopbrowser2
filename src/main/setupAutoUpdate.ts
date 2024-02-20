dee5df1d4ae91a51cdfd53a619776b4186175e3e
Dan-el Khen <danelkhen@gmail.com>
2024-02-20 13:44:45 +0100

    auto update

--- src/main/setupAutoUpdate.ts
+++ src/main/setupAutoUpdate.ts
@@ -7,77 +7,15 @@ import { autoUpdater } from "electron-updater"
 import { showMainWindow } from "./setupMainWindow"
 
 export function setupAutoUpdate() {
-    //-------------------------------------------------------------------
-    // Logging
-    //
-    // THIS SECTION IS NOT REQUIRED
-    //
-    // This logging setup is not required for auto-updates to work,
-    // but it sure makes debugging easier :)
-    //-------------------------------------------------------------------
     autoUpdater.logger = log
-    log.transports.file.level = "info"
+    // log.transports.file.level = "info"
     log.info("App starting...")
-
-    // //-------------------------------------------------------------------
-    // // Define the menu
-    // //
-    // // THIS SECTION IS NOT REQUIRED
-    // //-------------------------------------------------------------------
-    // const template: MenuItemConstructorOptions[] = []
-    // if (process.platform === "darwin") {
-    //     // OS X
-    //     const name = app.getName()
-    //     template.unshift({
-    //         label: name,
-    //         submenu: [
-    //             {
-    //                 label: "About " + name,
-    //                 role: "about",
-    //             },
-    //             {
-    //                 label: "Quit",
-    //                 accelerator: "Command+Q",
-    //                 click() {
-    //                     app.quit()
-    //                 },
-    //             },
-    //         ],
-    //     })
-    // }
-
-    //-------------------------------------------------------------------
-    // Open a window that displays the version
-    //
-    // THIS SECTION IS NOT REQUIRED
-    //
-    // This isn't required for auto-updates to work, but it's easier
-    // for the app to show a window than to have to click "About" to see
-    // that updates are working.
-    //-------------------------------------------------------------------
-    // let win: BrowserWindow | null = null
 
     async function sendStatusToWindow(text: string) {
         log.info(text)
         const win = await showMainWindow()
         win.webContents.send("message", text)
     }
-    // function createDefaultWindow() {
-    //     win = new BrowserWindow({
-    //         webPreferences: {
-    //             nodeIntegration: true,
-    //             contextIsolation: false,
-    //         },
-    //     })
-    //     // win.webContents.openDevTools()
-    //     win.on("closed", () => {
-    //         win = null
-    //     })
-    //     const url = `${baseUrl}/tray#v${app.getVersion()}`
-    //     log.info(url)
-    //     win.loadURL(url)
-    //     return win
-    // }
     autoUpdater.on("checking-for-update", () => {
         sendStatusToWindow("Checking for update...")
     })
@@ -98,64 +36,14 @@ export function setupAutoUpdate() {
     })
     autoUpdater.on("update-downloaded", () => {
         sendStatusToWindow("Update downloaded")
+        sendStatusToWindow("Quitting and installing...")
+        autoUpdater.quitAndInstall(true, true)
     })
-    // app.whenReady().then(() => {
-    //     // Create the Menu
-    //     const menu = Menu.buildFromTemplate(template)
-    //     Menu.setApplicationMenu(menu)
-
-    //     // createDefaultWindow()
-    // })
-    // app.on("window-all-closed", () => {
-    //     app.quit()
-    // })
-
-    //
-    // CHOOSE one of the following options for Auto updates
-    //
-
-    //-------------------------------------------------------------------
-    // Auto updates - Option 1 - Simplest version
-    //
-    // This will immediately download an update, then install when the
-    // app quits.
-    //-------------------------------------------------------------------
     app.whenReady().then(async () => {
         sendStatusToWindow(app.getVersion())
         log.info("checkForUpdatesAndNotify")
         const res = await autoUpdater.checkForUpdatesAndNotify()
         log.info("checkForUpdatesAndNotify result", res)
         sendStatusToWindow(JSON.stringify(res || "no res"))
-        // await sleep(10000)
-        // win?.close()
-        // win = null
     })
-
-    //-------------------------------------------------------------------
-    // Auto updates - Option 2 - More control
-    //
-    // For details about these events, see the Wiki:
-    // https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
-    //
-    // The app doesn't need to listen to any events except `update-downloaded`
-    //
-    // Uncomment any of the below events to listen for them.  Also,
-    // look in the previous section to see them being used.
-    //-------------------------------------------------------------------
-    // app.on('ready', function()  {
-    //   autoUpdater.checkForUpdates();
-    // });
-    // autoUpdater.on('checking-for-update', () => {
-    // })
-    // autoUpdater.on('update-available', (info) => {
-    // })
-    // autoUpdater.on('update-not-available', (info) => {
-    // })
-    // autoUpdater.on('error', (err) => {
-    // })
-    // autoUpdater.on('download-progress', (progressObj) => {
-    // })
-    // autoUpdater.on('update-downloaded', (info) => {
-    //   autoUpdater.quitAndInstall();
-    // })
 }
