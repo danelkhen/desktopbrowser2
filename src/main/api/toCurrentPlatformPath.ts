157ae1efa04935f2fa235caf24e1d9669aec1113
Dan-el Khen <danelkhen@gmail.com>
2024-03-02 12:04:09 +0100

    win32

--- src/main/api/toCurrentPlatformPath.ts
+++ src/main/api/toCurrentPlatformPath.ts
@@ -1,7 +1,12 @@
+import path from "path"
+
 export function toCurrentPlatformPath(p: string) {
     if (process.platform === "win32") {
         if (p.startsWith("//")) return p
-        if (p.startsWith("/")) return p.substring(1)
+        if (p.startsWith("/")) {
+            p = p.substring(1)
+        }
+        p = path.normalize(p)
     }
     return p
 }
--- src/main/api/toFile2.ts
+++ src/main/api/toFile2.ts
@@ -4,18 +4,6 @@ import path from "path"
 import { Stats } from "fs"
 
 export function globPathToFile(file: Path): IFile {
-    // if (typeof file === "string") {
-    //     const file2: IFile = {
-    //         Name: path.posix.basename(file),
-    //         IsFolder: true, // !!file.isDirectory(),
-    //         // Modified: file.mtime?.toJSON(),
-    //         // Size: file.isFile() ? file.size : undefined,
-    //         // IsHidden: file.name?.startsWith("."),
-    //         // Extension: path.extname(file.name),
-    //         Path: file,
-    //     }
-    //     return file2
-    // }
     const file2: IFile = {
         type: getType(file),
         Name: file.name,
@@ -29,20 +17,32 @@ export function globPathToFile(file: Path): IFile {
     return file2
 }
 
-export function toFile3(p: string, stat: Stats | null) {
-    const parsed = path.posix.parse(p)
-    const file2: IFile = {
-        type: getType(stat),
-        Name: parsed.name,
-        IsFolder: !!stat?.isDirectory(),
-        Modified: stat?.mtime?.toJSON(),
-        Size: stat?.isFile() ? stat.size : undefined,
-        IsHidden: parsed.name.startsWith("."),
-        Extension: path.posix.extname(parsed.name),
-        Path: p,
-    }
-    return file2
-}
+// if (typeof file === "string") {
+//     const file2: IFile = {
+//         Name: path.posix.basename(file),
+//         IsFolder: true, // !!file.isDirectory(),
+//         // Modified: file.mtime?.toJSON(),
+//         // Size: file.isFile() ? file.size : undefined,
+//         // IsHidden: file.name?.startsWith("."),
+//         // Extension: path.extname(file.name),
+//         Path: file,
+//     }
+//     return file2
+// }
+// export function toFile3(p: string, stat: Stats | null) {
+//     const parsed = path.posix.parse(p)
+//     const file2: IFile = {
+//         type: getType(stat),
+//         Name: parsed.name,
+//         IsFolder: !!stat?.isDirectory(),
+//         Modified: stat?.mtime?.toJSON(),
+//         Size: stat?.isFile() ? stat.size : undefined,
+//         IsHidden: parsed.name.startsWith("."),
+//         Extension: path.posix.extname(parsed.name),
+//         Path: p,
+//     }
+//     return file2
+// }
 function getType(stat: Path | Stats | null) {
     if (stat?.isDirectory()) {
         return "folder"
