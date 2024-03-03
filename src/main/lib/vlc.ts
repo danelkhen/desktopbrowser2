e9d8639ad5214ba25f448939b5049d2bb241091b
Dan-el Khen <danelkhen@gmail.com>
2024-03-03 15:41:04 +0100

    vlc

--- src/main/lib/vlc.ts
+++ src/main/lib/vlc.ts
@@ -2,6 +2,7 @@ import axios, { isAxiosError } from "axios"
 import child_process from "child_process"
 import { pathToFileURL } from "url"
 import { sleep } from "./sleep"
+import log from "electron-log/main"
 
 const exe =
     process.platform === "win32"
@@ -13,37 +14,36 @@ const host = "127.0.0.1"
 const user = ""
 
 export async function openVlc() {
-    await child_process.spawn(
-        `${exe} --extraintf http --http-host ${host} --http-port ${port} --http-password ${pwd}`,
-        {
-            detached: true,
-            shell: true,
-        }
-    )
+    const cmd = `${exe} --extraintf http --http-host ${host} --http-port ${port} --http-password ${pwd}`
+    const res = await child_process.spawn(cmd, { detached: true, shell: true })
+    log.info("openVlc", cmd, res)
 }
 
 export async function vlcPlay(file: string) {
-    if (process.platform === "darwin" && file.startsWith("/")) {
-        file = pathToFileURL(file).toString()
-    }
-    let res = await _vlcPlay(file)
+    const mrl = pathToFileURL(file).toString()
+    // if (process.platform === "darwin" && file.startsWith("/")) {
+    //     file = pathToFileURL(file).toString()
+    // }
+    let res = await _vlcPlay(mrl)
     if (res) return
     await sleep(500)
-    res = await _vlcPlay(file)
+    res = await _vlcPlay(mrl)
     if (res) return
     await openVlc()
     await sleep(1000)
-    res = await _vlcPlay(file)
+    res = await _vlcPlay(mrl)
     if (res) return
     throw new Error()
 }
-export async function _vlcPlay(file: string) {
+export async function _vlcPlay(mrl: string) {
     try {
         const auth = `${user}:${pwd}`
+        const params = { command: "in_play", input: mrl }
         const res = await axios(`http://${host}:${port}/requests/status.xml`, {
             headers: { Authorization: `Basic ${btoa(auth)}` },
-            params: { command: "in_play", input: file },
+            params,
         })
+        log.info("vlcPlay res", params, res)
         return res
     } catch (e) {
         return (isAxiosError(e) && e.response) ?? null
