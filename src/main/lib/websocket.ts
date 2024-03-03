1df46e136e3e5dd8879a0eed92557077ff48c992
Dan-el Khen <danelkhen@gmail.com>
2024-03-03 22:29:28 +0100

    logging

--- src/main/lib/websocket.ts
+++ src/main/lib/websocket.ts
@@ -2,28 +2,29 @@
 import * as http from "http"
 import * as ws from "ws"
 import { IWsReq } from "../../shared/IWsReq"
+import log from "electron-log/main"
 
 export function setupWebsockets(server: http.Server, service: any) {
-    console.log("setupWebsockets")
+    log.info("setupWebsockets")
     const wss = new ws.WebSocketServer({ path: "/api", server })
 
     wss.on("connection", (ws, req) => {
-        console.log("wss.onconnection", req.url)
+        log.info("wss.onconnection", req.url)
         ws.on("message", async message => {
             try {
                 const data = String(message)
-                console.log("ws.message received", data)
+                log.info("ws.message received", data)
                 const pc = JSON.parse(data) as IWsReq // extractFunctionCall(data)
                 const res = await service[pc.funcName](...pc.args)
                 if (isIterable(res)) {
-                    console.log("sending iterable!!!!!!!!!!!!!!!!")
+                    log.info("sending iterable!!!!!!!!!!!!!!!!")
                     ws.send("[")
                     for (const item of res) {
                         ws.send(JSON.stringify(item) + ",")
                     }
                     ws.send("]")
                 } else if (isAsyncIterable(res)) {
-                    console.log("sending async iterable!!!!!!!!!!!!!!!!!!")
+                    log.info("sending async iterable!!!!!!!!!!!!!!!!!!")
                     ws.send("[")
                     for await (const item of res) {
                         ws.send(JSON.stringify(item) + ",")
@@ -33,11 +34,11 @@ export function setupWebsockets(server: http.Server, service: any) {
                     ws.send(JSON.stringify(res))
                 }
             } catch (err) {
-                console.log(err)
+                log.warn(err)
                 ws.send("ERROR: " + JSON.stringify(err))
             }
         })
-        ws.on("error", e => console.log("ws.error", e))
+        ws.on("error", e => log.info("ws.error", e))
     })
 }
 
